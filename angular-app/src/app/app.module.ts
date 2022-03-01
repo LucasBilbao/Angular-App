160910ee2999e55f8ef7dd1562d72910d2345fde
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-01 16:03:12 +0400

    feat: implement module importing
    Import modules in the app module

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DocumentsModule } from './documents/documents-module/documents.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, DocumentsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
