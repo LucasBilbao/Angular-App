227202873d5d6d8a3852c5ff4d94fb426d9b9478
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-06 19:33:53 +0400

    feat: implement the interceptor in the app module

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DocumentsModule } from './documents/documents-module/documents.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CustomHttpInterceptor } from './app-routing/custom-http-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule, DocumentsModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CustomHttpInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
