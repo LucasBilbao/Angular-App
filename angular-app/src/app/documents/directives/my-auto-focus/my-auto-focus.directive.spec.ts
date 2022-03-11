f781e346ea6703ad6e56cea0d8e89b999c0505bc
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-11 14:49:52 +0400

    feat: implement my-auto-directive in share module

--- angular-app/src/app/documents/directives/my-auto-focus/my-auto-focus.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyAutoFocusDirective } from './my-auto-focus.directive';
-
-describe('MyAutoFocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyAutoFocusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
