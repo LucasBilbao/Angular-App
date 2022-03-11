ff60522c33ee2c7c147fd5371318821614794beb
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-11 18:06:58 +0400

    feat: implement style formatter comoponent
    Implement a style formatter component that formats the styles of the selected text. Functionalities: bold, italic, strikethrough, underline, and code.

---
+++ angular-app/src/app/context-menu/components/style-formatter/style-formatter.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ContextMenuItem } from '../../models/context-menu-item.model';
+import { ContextMenuService } from '../../services/context-menu.service';
+
+@Component({
+  selector: 'app-style-formatter',
+  templateUrl: './style-formatter.component.html',
+  styleUrls: ['./style-formatter.component.scss'],
+})
+export class StyleFormatterComponent implements OnInit {
+  @Input() styleItem: ContextMenuItem | null = null;
+  @Output() closeContextMenu = new EventEmitter();
+
+  constructor(private contextMenuService: ContextMenuService) {}
+
+  ngOnInit(): void {}
+
+  formatStyle(e: any): void {
+    const command = e.target.parentElement.id;
+
+    if (command === 'code') {
+      const selection = window.getSelection();
+      if (this.contextMenuService.isFormated(selection)) {
+        const selected = selection?.toString();
+        document.execCommand('insertHTML', false, '');
+        document.execCommand('insertHTML', false, `${selected}`);
+      } else {
+        document.execCommand('insertHTML', false, `<code>${selection}</code>`);
+      }
+    } else {
+      document.execCommand(command, false);
+    }
+
+    this.closeContextMenu.emit();
+  }
+}
