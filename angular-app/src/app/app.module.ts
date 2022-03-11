db72039908523ee7380e32ecaed21205d3c0f976
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-11 14:38:36 +0400

    feat: move the documents module
    Move the documents module and update the imports

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { DocumentsModule } from './documents/documents-module/documents.module';
+import { DocumentsModule } from './documents/documents.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CustomHttpInterceptor } from './app-routing/custom-http-interceptor';
 
