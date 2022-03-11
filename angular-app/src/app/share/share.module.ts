f3bc1c54e2cb11ab0e9934342e7e5076dcf6abbb
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-11 14:48:58 +0400

    feat: implement the directives in the share module
    Implement the directives in the share module to use it in different modules if needed.

--- angular-app/src/app/share/share.module.ts
+++ angular-app/src/app/share/share.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
+import { MyAutoFocusDirective } from './directives/my-auto-focus/my-auto-focus.directive';
+import { ClickStopPropagationDirective } from './directives/click-stop-propagation/click-stop-propagation.directive';
 
 @NgModule({
-  declarations: [LoaderComponent],
-  exports: [LoaderComponent],
+  declarations: [
+    LoaderComponent,
+    MyAutoFocusDirective,
+    ClickStopPropagationDirective,
+  ],
+  exports: [
+    LoaderComponent,
+    MyAutoFocusDirective,
+    ClickStopPropagationDirective,
+  ],
   imports: [CommonModule],
 })
 export class ShareModule {}
