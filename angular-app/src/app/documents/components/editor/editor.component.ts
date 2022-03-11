5823efa27cd7b6d1e2989bec2c51c683af60a025
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-11 16:42:18 +0400

    feat: implement a rich editor and context menu
    Implement a rich text editor instead of the text area that was implemented before. It allows text formatting. Also, implement the context menu component in the editor component, which opens when the user right clicks on the editor.

--- angular-app/src/app/documents/components/editor/editor.component.ts
+++ angular-app/src/app/documents/components/editor/editor.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  ContextMenu,
+  defaultContextMenu,
+} from 'src/app/context-menu/models/context-menu.model';
+import { ContextMenuService } from 'src/app/context-menu/services/context-menu.service';
 import { DocumentItem } from '../../models/document.model';
 import { DocumentService } from '../../services/document/document.service';
 
@@ -8,9 +14,54 @@ import { DocumentService } from '../../services/document/document.service';
   styleUrls: ['./editor.component.scss'],
 })
 export class EditorComponent implements OnInit {
-  @Input() document: DocumentItem | null = null;
+  document: DocumentItem | null = null;
+  contextMenuDetails: ContextMenu = defaultContextMenu;
 
-  constructor(private documentService: DocumentService) {}
+  value: string | undefined = '';
 
-  ngOnInit(): void {}
+  constructor(
+    private documentService: DocumentService,
+    private activatedRoute: ActivatedRoute,
+    private contextMenuService: ContextMenuService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.documentService.setActiveID(params['id']);
+      this.document = this.documentService.getDocumentByID(params['id']);
+      this.value = this.document?.description;
+    });
+  }
+
+  onInput(e: any): void {
+    const document: DocumentItem | null = this.document;
+    const inputValue = e.target.innerHTML;
+
+    if (document) {
+      document.description = inputValue;
+      this.documentService.putUpdateDocument(document);
+    }
+  }
+
+  test(e: any): void {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  // These methods serve the context menu
+
+  onContextMenu(e: any): void {
+    e.preventDefault();
+
+    this.contextMenuDetails = {
+      x: e.clientX,
+      y: e.clientY,
+      isActive: true,
+    };
+  }
+
+  contextMenuOff(): void {
+    if (this.contextMenuDetails?.isActive)
+      this.contextMenuDetails = defaultContextMenu;
+  }
 }
