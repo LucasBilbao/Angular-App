1207bc78c4f5b3888fd9d8026f251943209f9329
Luka Bilbao-Munios <77238294+LucasBilbao@users.noreply.github.com>
2022-03-11 18:17:25 +0400

    feat: implement the context menu module

--- angular-app/src/app/context-menu/context-menu.module.ts
+++ angular-app/src/app/context-menu/context-menu.module.ts
@@ -4,9 +4,16 @@ import { CoreModule } from '../core/core.module';
 import { ShareModule } from '../share/share.module';
 import { ContextMenuComponent } from './components/context-menu/context-menu.component';
 import { LinkInputComponent } from './components/link-input/link-input.component';
+import { StyleFormatterComponent } from './components/style-formatter/style-formatter.component';
+import { FontStylerComponent } from './components/font-styler/font-styler.component';
 
 @NgModule({
-  declarations: [ContextMenuComponent, LinkInputComponent],
+  declarations: [
+    ContextMenuComponent,
+    LinkInputComponent,
+    StyleFormatterComponent,
+    FontStylerComponent,
+  ],
   imports: [CommonModule, CoreModule, ShareModule],
   exports: [ContextMenuComponent, LinkInputComponent],
 })
